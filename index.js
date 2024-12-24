664c1570dbd205999c0fa18362f67d01e7aadfea
MD Monirujjaman <monirbinkhaled1@gmail.com>
2024-12-24 11:42:16 +0600

    1st video done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+// let array = [78,89,54,23,35];
+// console.log(array);
+// array.unshift(21);
+// console.log(array);
+// let x = array.shift();
+// console.log(array);
+// console.log(x);
+
+// array.pop()
+// console.log(array);
+// let x = array.pop();
+// console.log(x);
+// console.log(array);
+
+// array.pop()
+// array.pop()
+// console.log(array.length);
+// array.push(200,100,56,312,687,978)
+// console.log(array.length);
+// console.log(array[3]);
+// console.log(array.length);
+// let a = 09;
+
+let x = ['aam','jam','kola'];
+let y = ['golap','joba','kola','bali.'];
+let j = ["monir","moyna","rokshana","khaled"];
+let k = [10,32,46,45,98];
+let u = j.concat(k,x,y);
+let find = u.indexOf('bali');
+console.log(u);
+console.log(find);
+// console.log(u[8]);
+// console.log(typeof u);

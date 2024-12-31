eb9ba2ee0bf3817c48ab4636a06b1541ff4a2cdb
MD Monirujjaman <monirbinkhaled1@gmail.com>
2024-12-31 11:53:05 +0600

    added html

--- index.js
+++ index.js
@@ -21,13 +21,94 @@
 // console.log(array.length);
 // let a = 09;
 
-let x = ['aam','jam','kola'];
-let y = ['golap','joba','kola','bali.'];
-let j = ["monir","moyna","rokshana","khaled"];
-let k = [10,32,46,45,98];
-let u = j.concat(k,x,y);
-let find = u.indexOf('bali');
-console.log(u);
-console.log(find);
+// let x = ['aam','jam','kola'];
+// let y = ['golap','joba','kola','bali.'];
+// let j = ["monir","moyna","rokshana","khaled"];
+// let k = [10,32,46,45,98];
+// let u = j.concat(k,x,y);
+// let find = u.indexOf('bali');
+// console.log(u);
+// console.log(find);
 // console.log(u[8]);
 // console.log(typeof u);
+
+
+// for(let i = 1; i<50; i--){
+//   // console.log("Monir",i);
+// };
+
+// let i = 10;
+// while(i <= 15){
+//   // console.log('Monir')
+//   i--
+// }
+
+// let i = 0;
+// do{
+//   console.log(i);
+//   i++;
+// } while(i <= 5);
+
+// let furit = ['aam','jam','kola','kathal','lechu'];
+// for(fu of furit){
+//   console.log(fu);
+//   // furit++;
+// }
+
+// let numbers = [21,23,65,45,87,98];
+
+// for(num of numbers.reverse()){
+//   console.log(num)
+// };
+
+
+// let iam ="my name is monir"
+// for(i of iam){
+//   console.log(i);
+// }
+
+// let furits = ['apple','banana','mango'];
+// for(furit in furits){
+//   console.log(furit)
+// }
+// for(furit of furits){
+//   console.log(furit)
+// }
+
+// let name = "Monir"
+// for (const char of name) {
+//   console.log(char);
+// }
+
+
+// console.log(mySet); // Output: Set { 1, 2, 3 }
+// const mySet = new Set([1, 2, 3, 3]);
+// for ( value of mySet) {
+//   console.log(value);
+// };
+
+
+// let enyThings = new Set([12,32,45,65,98,87,45,65,78]);
+// for(val of enyThings){
+//   console.log(val);
+// }
+
+
+let typedArray =([12.00,32.05,45.32,65.56,98.45,87.25,45.41,65.89,78.78]);
+// for(){
+
+// }
+// console.log(typedArray[0]);
+// for(value of typedArray){
+//   console.log(value);
+// }
+
+
+function printArgs() {
+  for (const arg of arguments) {
+      console.log(arg);
+  }
+}
+printArgs("Hello", "World", 2024);
+
+
